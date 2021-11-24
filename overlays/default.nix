98bb0147e59fc785e244c84ecb8b3049ab5a304e
Nik Steffen <nik.steffen@pm.me>
2021-11-24 21:19:02 +0100

    removes hashicorp content

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,10 +2,7 @@
  * other ".nix" files in this directory are automatically loaded.
  */
 self: super: {
-  consul-bin = self.callPackage ../pkgs/consul-bin.nix {};
   create-dmg = self.callPackage ../pkgs/create-dmg.nix {};
-  nomad-bin = self.callPackage ../pkgs/nomad-bin.nix {};
-  terraform-bin = self.callPackage ../pkgs/terraform-bin.nix {};
 
   # Have to force Go 1.17 because the default is fixed to 1.16
   # for reasons in the nixpkgs repository. We'll undo this when
